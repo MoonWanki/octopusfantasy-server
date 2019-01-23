9c9e7fb96b101b62fa705b7ac77223357d47683f
aibees <aibees1129@naver.com>
2019-01-24 02:33:55 +0900

    helmet module add

--- index.js
+++ index.js
@@ -1,33 +1,45 @@
 require('dotenv').config();
 
+// module definition
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const helmet = require('helmet');
 
+// module
 const app = express();
 const port = process.env.PORT || 4500;
 
+//static add
 app.use(express.static('public'));
 
+//bodyparser add
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//global promise
 mongoose.Promise = global.Promise;
 
+//Database connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
     .then(() => console.log('MongoDB Connected'))
     .catch(e => console.error(e));
 
 //session
 app.use(session({
+    name : 'OctopusyFantasy',
     secret : 'encryptionPrivateKey',
     resave : true,
     saveUninitialized : true,
     store : require('mongoose-session')(mongoose)
-}))
+}));
 
+//security
+    //helmet
+app.use(helmet.hidePoweredBy({setTo : 'YouCantSeeMe'}))
+    //cors
 app.use(cors());
 
 app.use('/post', require('./route/post'));
