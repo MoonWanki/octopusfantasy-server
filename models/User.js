154b2fce02c24d73606775bfdc68f784ff555a97
MoonWanki <dhksrl2589@gmail.com>
2019-02-14 16:29:53 +0900

    Fix git config ignorecase to false

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    'id' : String,
+    'nickname' : String,
+    'profileImage' : String,
+    'email' : String,
+});
+
+UserSchema.statics.getAllUsers = () => {
+    return this.find({})
+}
+
+UserSchema.statics.getUserById = function(id) {
+    return this.find({ 'id': id })
+}
+
+UserSchema.statics.updateUser = function(profile) {
+    return this.findOneAndUpdate(
+        { id: profile.id },
+        profile,
+        { upsert: true }
+    )
+}
+
+UserSchema.statics.deleteUser = function(id) {
+    return this.findByIdAndDelete(id)
+}
+
+module.exports = mongoose.model('user', UserSchema);
