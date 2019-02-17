d57e465d3288dae2199a808e52d49c78bb5d0cf7
Aibees <aibees1129@naver.com>
2019-02-18 00:01:45 +0900

    comment

--- socketio/OctoMahjong/Hand.js
+++ socketio/OctoMahjong/Hand.js
@@ -19,7 +19,7 @@ class Hand {
             if(idx != -1) this.closed[idx] = this.tsumoTile
             this.tsumoTile = null
         }
-    } //여기 수정하세요
+    } //여기 수정하세요 쯔모한거 버릴수도 있어요
     
     isClosed() {
         return this.opened.length == 0
@@ -29,7 +29,8 @@ class Hand {
         const tiles = tiles || this.closed
 
         // TODO: find winnable tiles if tenpai.
-
+        // 텐파이는 그냥 단순히 머리1개, 몸통 4개 만들 수 있는지
+        // 화료는 역이 있는지
         return null // winnable tiles or empty array
     }
 }
