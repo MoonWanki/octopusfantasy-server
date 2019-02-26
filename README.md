f4feac6ad7f29cd1b8e7ae94fa167d3cd50c59f5
MoonWanki <dhksrl2589@gmail.com>
2019-02-26 23:10:41 +0900

    Update unauthorized request handler to post api

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # OctopusFantasy Server
 
-Main server for [**Octopus Fantasy**](https://www.octopusfantasy.com) which includes **RESTful API**, **OAuth**, and **WebSocket** for [**Octo Mahjong Online**](https://mahjong.octopusfantasy.com).
+[![Express](https://img.shields.io/badge/Express-v4.16.4-red.svg?style=flat-square)](https://reactjs.org/)
+[![Socket.IO](https://img.shields.io/badge/Socket.IO-v4.0.1-yellowgreen.svg?style=flat-square)](https://socket.io/)
+[![MongoDB](https://img.shields.io/badge/Mongoose-v5.4.3-47A248.svg?style=flat-square&logo=MongoDB)](https://material-ui.com/)
+
+Main server for [**Octopus Fantasy**](https://www.octopusfantasy.com) which includes **RESTful API**, **OAuth**, and **SocketIO** for [**Octo Mahjong Online**](https://mahjong.octopusfantasy.com).
 
 ## Octopus Fantasy
 
@@ -11,6 +15,23 @@ Personal platform made by MoonWanki. Personal blog as primary contents.
 
 ## Octo Mahjong Online
 
-Cross-platform online mahjong game via HTML5 websocket.  
+Cross-platform online mahjong game via SocketIO.  
   
 **Official website** | https://mahjong.octopusfantasy.com  
+
+## Dependencies  
+
+- **Express** | https://expressjs.com  
+Fast, unopinionated, minimalist web framework.  
+
+- **``Socket.IO``** | https://socket.io  
+Provides real-time, bi-directional communication between web clients and server.  
+
+- **Mongoose** | https://mongoosejs.com  
+Provides elegant mongodb object modeling.  
+
+- **Express-Session** | https://github.com/expressjs/session  
+Session middleware for both Express and SocketIO.  
+
+- **Axios** | https://github.com/axios/axios  
+Promise based HTTP client.  
--- route/post.js
+++ route/post.js
@@ -1,5 +1,5 @@
-const router = require('express').Router();
-const Post = require('../models/Post');
+const router = require('express').Router()
+const Post = require('../models/Post')
 
 router.get('/', (req, res) => {
     const { type } = req.query
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
     else {
         Post.getAllPosts()
         .then(posts => res.send(posts))
-        .catch(err => res.status(500).send(err));
+        .catch(err => res.status(500).send(err))
     }
 })
 
@@ -19,51 +19,76 @@ router.get('/:pid', (req, res) => {
     const { pid } = req.params
     Post.getPostById(pid)
     .then(post => res.send(post))
-    .catch(err => res.status(500).send(err));
+    .catch(err => res.status(500).send(err))
 })
 
 /* --------------- LIKE --------------- */
 
-router.post('/:pid/likes', (req, res) => {
-    const { pid } = req.params
-    const { uid } = req.body
-    Post.addLike(pid, uid)
-    .then(post => res.send(post))
-    .catch(err => res.status(500).send(err));
+router.post('/:pid/like', (req, res) => {
+    if(req.session.user) {
+        const { pid } = req.params
+        const { id: uid } = req.session.user
+        Post.addLike(pid, uid)
+        .then(post => res.send(post))
+        .catch(err => res.status(500).send(err))
+    } else {
+        res.sendStatus(401)
+    }
 })
 
-router.delete('/:pid/likes', (req, res) => {
-    const { pid } = req.params
-    const { uid } = req.body
-    Post.deleteLike(pid, uid)
-    .then(post => res.send(post))
-    .catch(err => res.status(500).send(err));
+router.delete('/:pid/like', (req, res) => {
+    if(req.session.user) {
+        const { pid } = req.params
+        const { id: uid } = req.session.user
+        Post.deleteLike(pid, uid)
+        .then(post => res.send(post))
+        .catch(err => res.status(500).send(err))
+    } else {
+        res.sendStatus(401)
+    }
 })
 
 /* --------------- COMMENT --------------- */
 
 router.post('/:pid/comment', (req, res) => {
-    const { pid } = req.params
-    const { uid, text } = req.body
-    Post.addComment(pid, uid, text)
-    .then(post => res.send(post))
-    .catch(err => res.status(500).send(err))
+    if(req.session.user) {
+        const { id: uid } = req.session.user
+        const { pid } = req.params
+        const { text } = req.body
+        Post.addComment(pid, uid, text)
+        .then(post => res.send(post))
+        .catch(err => res.status(500).send(err))
+    } else {
+        res.sendStatus(401)
+    }
 })
 
 router.put('/:pid/comment', (req, res) => {
-    const { pid } = req.params
-    const { uid, cid, text } = req.body
-    Post.editComment(pid, cid, text)
-    .then(post => res.send(post))
-    .catch(err => res.status(500).send(err))
+    if(req.session.user) {
+        const { id: uid } = req.session.user
+        // TODO: check if the comment is made by this user
+        const { pid } = req.params
+        const { cid, text } = req.body
+        Post.editComment(pid, cid, text)
+        .then(post => res.send(post))
+        .catch(err => res.status(500).send(err))
+    } else {
+        res.sendStatus(401)
+    }
 })
 
 router.delete('/:pid/comment', (req, res) => {
-    const { pid } = req.params
-    const { uid, cid } = req.body
-    Post.deleteComment(pid, cid)
-    .then(post => res.send(post))
-    .catch(err => res.status(500).send(err))
+    if(req.session.user) {
+        const { id: uid } = req.session.user
+        // TODO: check if the comment is made by this user
+        const { pid } = req.params
+        const { cid } = req.body
+        Post.deleteComment(pid, cid)
+        .then(post => res.send(post))
+        .catch(err => res.status(500).send(err))
+    } else {
+        res.sendStatus(401)
+    }
 })
 
-module.exports = router;
+module.exports = router
