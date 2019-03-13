e11fb3e3c98b4a544b699a6f359e44f679bcad27
aibees <aibees1129@naver.com>
2019-03-13 16:34:55 +0900

    Create getYakus function

--- socketio/OctoMahjong/utils/getResult.js
+++ socketio/OctoMahjong/utils/getResult.js
@@ -3,14 +3,27 @@
 module.exports = (rotation, player) => {
     const { wind, round, turn } = rotation
     const { didRiichi, didDoubleRiichi, wind: playerWind } = player
-    const { closed, opened, tsumoTile } = player.hand
+    const { closed, opened, tsumoTile, agariTiles } = player.hand
 
-    const yakus = new Array()
+    let candidate = new Array()
 
+    for(var agari of agariTiles) {
+        const {tile} = agari
+        if(JSON.stringify(tile) == JSON.stringify(tsumoTile)) {
+            candidate.push(getYakus(agari, closed, opened))
+        }
+    }
+
+
+    return yakus
+}
+
+const getYakus = (agari, closed, opend) => {
+    let yakus = new Array()
+    const {melds, head, tile} = agari
+    
     if(didRiichi) yakus.push('riichi')
     if(didDoubleRiichi) yakus.push('double riichi')
-
-    // TODO: push all satisfied yakus
 
     return yakus
 }
